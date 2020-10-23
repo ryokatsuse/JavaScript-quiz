afdfe04e09d52c3e2b9c2261ba083801600ea3b6
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-10-23 11:05:37 +0900

    feat:13日目

---
+++ 13day/41.js
@@ -0,0 +1,7 @@
+console.log(!!null) // false
+console.log(!!"") // false
+console.log(!!1) // true
+
+// falsyかどうかで決まる　nullはfalsy, ""はfalsy, 1はtruthy
+
+
---
+++ 13day/42.js
@@ -0,0 +1,4 @@
+setInterval(() => console.log ("Hi"), 1000)
+
+// 一意のIDを返します。IDはclearInterval()関数でその感覚をクリアするために使うことができる
+
